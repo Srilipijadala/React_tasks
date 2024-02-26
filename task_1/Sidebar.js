08e42ca1164b02e139392fa5a44e46c70245ddb6
Srilipijadala <132419035+Srilipijadala@users.noreply.github.com>
2024-02-25 22:42:41 -0600

    Sidebar.js

--- task_1/Sidebar.js
+++ task_1/Sidebar.js
@@ -1,1 +1,15 @@
+import React from 'react'
+function Sidebar()
+{
+    return(
+       <div>
+        <aside className="sidebar">
+            <h1>Child2 Sidebar Component</h1>
+        </aside>
+       </div>   
+        
+    )
+    
+}
 
+export default Sidebar

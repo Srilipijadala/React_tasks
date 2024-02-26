36eb2e8629510e2a48ec403786ea78bc784edf15
Srilipijadala <132419035+Srilipijadala@users.noreply.github.com>
2024-02-25 22:43:44 -0600

    Create MainContent.js

---
+++ task_1/MainContent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+function MainContent()
+{
+    return(
+        <div>
+        <main className="maincontent">
+            <h1>Child3 Main Component</h1>
+        </main>
+        </div>
+    ) 
+    
+}
+
+export default MainContent

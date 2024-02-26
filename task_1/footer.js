372515fd7b28b85ad00b9ca110fdd748302f3292
Srilipijadala <132419035+Srilipijadala@users.noreply.github.com>
2024-02-25 22:36:01 -0600

    footer.js

---
+++ task_1/footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+function Footer()
+{
+    return(
+        <div>
+            <footer className="footer">
+                <h1>Child4 Footer Component</h1>
+            </footer>
+        </div>
+        
+    ) 
+    
+}
+
+export default Footer

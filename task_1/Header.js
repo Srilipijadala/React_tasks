1ca5402045b190562219ccbb52b8073101f50eb0
Srilipijadala <132419035+Srilipijadala@users.noreply.github.com>
2024-02-25 22:37:03 -0600

    Create Header.js

---
+++ task_1/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+function Header()
+{
+    return(
+        <div>
+        <header className="header">
+            <h1>Child1 Header Component</h1>
+        </header>
+        </div>
+    ) 
+    
+}
+
+export default Header
